1b862f60a20e19080b47002a95c3b6cca9ed37e2
Joyvalley <j.a.freudenthal@gmail.com>
2017-09-21 11:08:26 +0200

    looking for bugs but it seems to work fine

--- benchmarkgwas.r
+++ benchmarkgwas.r
@@ -1,11 +1,17 @@
 suppressPackageStartupMessages(require("doMC"))
 setwd("/storage/evolgen/jan/Gwas")
 
+setwd("/storage/evolgen/jan/Gwas/GWAS")
 #source("t2029_par.r")
-source("t2029_gwas.r")
+source("/storage/evolgen/jan/Gwas/t2029_gwas.r")
+source("emma.r")
+source("gwas_mc.r")
+setwd("../")
 
 my_dorm <- read.csv("Dormancy.csv")
 my_dorm <- my_dorm[,c(2,8)]
+
+
 
 time_sc <- Sys.time()
 results_sc <- t2029_gwas(my_dorm, u.max = 1, mc= FALSE)
@@ -13,6 +19,7 @@ jane_sc <- Sys.time() - time_sc
 
 time_mc <- Sys.time()
 results_mc <- t2029_gwas(my_dorm,u.max = 1, mult.core = TRUE)
+
 jane_mc <- Sys.time() - time_mc
 
 print(jane_mc)
@@ -22,16 +29,8 @@ save(results_mc,jane_mc, file = 'bench_mc.rda')
 save(results_sc,jane_sc, file= 'bench_sc.rda')
 
 
-load("X_2029_1.rda")
-X.rda <- X
-X.rds <- readRDS("X_2029_1.rds")
-
-
-
-time <- Sys.time()
-load("X_2029_1.rda")
-jane <- Sys.time() - time 
-
+system.time(load("X_2029_dt_1.rda"))
+system.time(load("X_2029_1.rda"))
 
 
 
