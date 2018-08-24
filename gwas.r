73974c57830d4b512f5926ba010d4ba58846a3cc
Joyvalley <j.a.freudenthal@gmail.com>
2018-08-24 11:03:17 +0200

    indentation and spacing

--- gwas.r
+++ gwas.r
@@ -31,185 +31,160 @@
 
 amm_gwas<-function(Y,X,K,p=0.001,m=2,run=TRUE,calculate.effect.size=FALSE,include.lm=FALSE,include.kw=FALSE,use.SNP_INFO=FALSE,update.top_snps=FALSE,report=TRUE, mc = FALSE, cores = 10) {
 
-stopifnot(is.numeric(Y[,1]))
-Y_<-Y[order(Y[,1]),]
-Y<-as.matrix(Y_[,m])
-rownames(Y)<-as.integer(Y_[,1])
-Y<-na.omit(Y)
-XX<-X[rownames(X) %in% rownames(Y),]
-if (report==TRUE) {
-cat ('GWAS performed on', length(which(rownames(Y)%in%rownames(X))),'ecotypes, ', nrow(Y)-length(which(rownames(Y)%in%rownames(X))),'values excluded','\n')
-}
+    stopifnot(is.numeric(Y[,1]))
+    Y_ <- Y[order(Y[,1]),]
+    Y <- as.matrix(Y_[,m])
+    rownames(Y) <- as.integer(Y_[,1])
+    Y <- na.omit(Y)
+    XX <- X[rownames(X) %in% rownames(Y),]
+    if(report == TRUE) {
+        cat ('GWAS performed on', length(which(rownames(Y)%in%rownames(X))),'ecotypes, ', nrow(Y)-length(which(rownames(Y)%in%rownames(X))),'values excluded','\n')
+    }
 
-if (use.SNP_INFO==FALSE){ 
-options(stringsAsFactors = FALSE)
-  if(report==TRUE) {
-cat('SNP_INFO file created','\n')}
-SNP_INFO<-data.frame(cbind(colnames(X),matrix(nrow=ncol(X),ncol=2,data=unlist(strsplit(colnames(X),split='- ')),byrow=T)))
-colnames(SNP_INFO)<-c('SNP','Chr','Pos')
-SNP_INFO[,2]<-as.numeric(SNP_INFO[,2])
-SNP_INFO[,3]<-as.numeric(SNP_INFO[,3])
-} else { if (report==TRUE) {cat('User definied SNP_INFO file is used','\n')}}
-
-
-Y1<-as.matrix(Y[rownames(Y) %in% rownames(XX),])
-colnames(Y1)<-colnames(Y)
-rownames(Y1)<-rownames(Y)[rownames(Y)%in%rownames(XX)]
-ecot_id<-as.integer(rownames(Y1))
+    if(use.SNP_INFO == FALSE) { 
+        options(stringsAsFactors = FALSE)
+        if(report == TRUE) {
+            cat('SNP_INFO file created','\n')
+        }
+        SNP_INFO<-data.frame(cbind(colnames(X),matrix(nrow=ncol(X),ncol=2,data=unlist(strsplit(colnames(X),split='- ')),byrow=T)))
+        colnames(SNP_INFO) <-c('SNP','Chr','Pos')
+        SNP_INFO[,2]<-as.numeric(SNP_INFO[,2])
+        SNP_INFO[,3]<-as.numeric(SNP_INFO[,3])
+    } else {
+        if(report == TRUE) { cat('User definied SNP_INFO file is used','\n')}}
 
 
-K1<-K[rownames(K) %in% ecot_id,]
+    Y1 <- as.matrix(Y[rownames(Y) %in% rownames(XX),])
+    colnames(Y1) <- colnames(Y)
+    rownames(Y1) <- rownames(Y)[rownames(Y)%in%rownames(XX)]
+    ecot_id <- as.integer(rownames(Y1))
 
-K2<-K1[,colnames(K1) %in% ecot_id]
+    K1 <- K[rownames(K) %in% ecot_id,]
+    K2 <- K1[,colnames(K1) %in% ecot_id]
+    K_ok <- as.matrix(K2)
 
-K_ok<-as.matrix(K2)
+    a <- rownames(K_ok)
 
-a<-rownames(K_ok)
+    n <- length(a)
+    K_stand <- (n-1)/sum((diag(n)-matrix(1,n,n)/n)*K_ok)*K_ok
 
-n<-length(a)
-K_stand<-(n-1)/sum((diag(n)-matrix(1,n,n)/n)*K_ok)*K_ok
+    Y <- Y1[which(rownames(Y1)%in%a),]
+    X_ <- XX[which(rownames(XX)%in%a),]
 
-Y<-Y1[which(rownames(Y1)%in%a),]
-X_<-XX[which(rownames(XX)%in%a),]
+    rm(X,XX)
+    gc()
 
-rm(X,XX)
-gc()
-
-
-AC_1 <- data.frame(colnames(X_),apply(X_,2,sum))
-colnames(AC_1)<-c('SNP','AC_1')
-
-MAF_1<-data.frame(AC_1,AC_0=nrow(X_)-AC_1$AC_1)
-
-MAF_2<-data.frame(MAF_1,MAC=apply(MAF_1[,2:3],1,min))
-
-MAF_3<-data.frame(MAF_2,MAF=(MAF_2$MAC/nrow(X_)))
-
-MAF_ok<-merge(SNP_INFO,MAF_3,by='SNP')
-
-rm(AC_1,MAF_1,MAF_2,MAF_3)
+    
+    AC_1 <- data.frame(colnames(X_),apply(X_,2,sum))
+    colnames(AC_1) <- c('SNP','AC_1')
+    MAF_1 <- data.frame(AC_1,AC_0=nrow(X_)-AC_1$AC_1)
+    MAF_2 <- data.frame(MAF_1,MAC=apply(MAF_1[,2:3],1,min))
+    MAF_3 <- data.frame(MAF_2,MAF=(MAF_2$MAC/nrow(X_)))
+    MAF_ok <- merge(SNP_INFO,MAF_3,by='SNP')
+    rm(AC_1,MAF_1,MAF_2,MAF_3)
 
 #Filter for MAF
 
-MAF<-subset(MAF_ok,MAF<p)[,1]
-
-X_ok<-X_[,!colnames(X_) %in% MAF]
-
-rm(MAF,X_)
-
-#REML
-
-Xo<-rep(1,nrow(X_ok))
-ex<-as.matrix(Xo)
-
-
-null<-emma.REMLE(Y,ex,K_stand)
-
-herit<-null$vg/(null$vg+null$ve)
-if (report==TRUE) {
-cat('pseudo-heritability estimate is ',herit,'\n')
-}
-if (run==FALSE) {
-
-cat('no GWAS performed','\n') } else {
-
-M<-solve(chol(null$vg*K_stand+null$ve*diag(dim(K_stand)[1])))
-
-Y_t<-crossprod(M,Y)
-
-int_t<-crossprod(M,(rep(1,length(Y))))
-
-
-
-if (calculate.effect.size==TRUE) {
-models1<-apply(X_ok,2,function(x){summary(lm(Y_t~0+int_t+crossprod(M,x)))$coeff[2,]})
-
-
-
-
-bet<-models1[1,]
-### variance explained from betas veb   !super slow this way!
-# bet^2*var(X_ok[,t])/var(Y[,n]) =  veb/(1-veb)
-veb<- bet[1]^2*var(X_ok[,1])/var(Y)/(1+bet[1]^2*var(X_ok[,1])/var(Y))
-for ( t in 2:ncol(X_ok)) {
- veb<-c(veb,bet[t]^2*var(X_ok[,t])/var(Y)/(1+bet[t]^2*var(X_ok[,t])/var(Y)))
-}
-# similar (but slightly different) to RSS method below or to adjusted R2 from lm call 
-out_models<-data.frame(SNP=colnames(models1),Pval=models1[4,],variance_explained=veb,beta=bet)
-
-} else {
-
-#EMMAX SCAN
-RSS_env<-rep(sum(lsfit(int_t,Y_t,intercept = FALSE)$residuals^2),ncol(X_ok))
-
-if(mc==FALSE){
-R1_full<-apply(X_ok,2,function(x){sum(lsfit(cbind(int_t,crossprod(M,x)),Y_t,intercept = FALSE)$residuals^2)})
-}else{
-    if(!require(doMC)) install.packages("doMC")
-    require(doMC)
-    if( cores == "all") { 
-        registerDoMC(system("cat /proc/cpuinfo  | grep processor  | wc -l", intern = T))
-    }else {
-        registerDoMC(cores)
-    }
-    R1_full <- foreach(i = 1:ncol(X_ok), .combine = "c") %dopar%
-        sum(lsfit(cbind(int_t,crossprod(M,X_ok[,i])),Y_t,intercept = FALSE)$residuals^2)
-}
+    MAF <- subset(MAF_ok,MAF<p)[,1]
+    X_ok <- X_[,!colnames(X_) %in% MAF]
+    rm(MAF,X_)
     
-pa<-nrow(Y1)
+# REML
 
-F_1<-((RSS_env-R1_full)/1)/(R1_full/(pa-3))
-pval_Y1<-pf(F_1,1,(pa-2),lower.tail=FALSE)
+    Xo <- rep(1,nrow(X_ok))
+    ex <- as.matrix(Xo)
 
-snp<-colnames(X_ok)
-out_models<-data.frame(SNP=snp,Pval=pval_Y1,variance_explained=1-R1_full/RSS_env)}
- 
-output<-merge(MAF_ok,out_models,by='SNP')
+    null <- emma.REMLE(Y,ex,K_stand)
+    herit <- null$vg/(null$vg+null$ve)
 
-if (include.lm==TRUE) {
-RSS_env_<-rep(sum(lsfit(rep(1,length(Y)),Y,intercept = FALSE)$residuals^2),ncol(X_ok))
-R1_full_<-apply(X_ok,2,function(x){sum(lsfit(x,Y,intercept = T)$residuals^2)})
-pa<-nrow(Y1)
+    if(report == TRUE) {
+        cat('pseudo-heritability estimate is ',herit,'\n')
+    }
+    if(run == FALSE) {
+        cat('no GWAS performed','\n')
+    } else {
+        M <- solve(chol(null$vg*K_stand+null$ve*diag(dim(K_stand)[1])))
+        Y_t <- crossprod(M,Y)
+        int_t <- crossprod(M,(rep(1,length(Y))))
+        
+        if(calculate.effect.size == TRUE) {
+            models1 <- apply(X_ok,2,function(x){summary(lm(Y_t~0+int_t+crossprod(M,x)))$coeff[2,]})
+            bet <- models1[1,]
+### variance explained from betas veb   !super slow this way!
+                                        # bet^2*var(X_ok[,t])/var(Y[,n]) =  veb/(1-veb)
+            veb <- bet[1]^2*var(X_ok[,1])/var(Y)/(1+bet[1]^2*var(X_ok[,1])/var(Y))
+            for( t in 2:ncol(X_ok)) {
+                veb <- c(veb,bet[t]^2*var(X_ok[,t])/var(Y)/(1+bet[t]^2*var(X_ok[,t])/var(Y)))
+            }
+                                        # similar (but slightly different) to RSS method below or to adjusted R2 from lm call 
+            out_models<-data.frame(SNP=colnames(models1),Pval=models1[4,],variance_explained=veb,beta=bet)
 
-F_1_<-((RSS_env_-R1_full_)/1)/(R1_full_/(pa-3))
-pval_Y1_lm<-pf(F_1_,1,(pa-3),lower.tail=FALSE)
+        } else {
+             #EMMAX SCAN
+            RSS_env <- rep(sum(lsfit(int_t,Y_t,intercept = FALSE)$residuals^2),ncol(X_ok))
+            
+            if(mc == FALSE) {
+                R1_full <- apply(X_ok,2,function(x){sum(lsfit(cbind(int_t,crossprod(M,x)),Y_t,intercept = FALSE)$residuals^2)})
+            } else {
+                if(!require(doMC)) install.packages("doMC")
+                require(doMC)
+                if(cores == "all") { 
+                    registerDoMC(system("cat /proc/cpuinfo  | grep processor  | wc -l", intern = T))
+                } else {
+                    registerDoMC(cores)
+                }
+                R1_full <- foreach(i = 1:ncol(X_ok), .combine = "c") %dopar%
+                    sum(lsfit(cbind(int_t,crossprod(M,X_ok[,i])),Y_t,intercept = FALSE)$residuals^2)
+            }
+            
+            pa<-nrow(Y1)
+            
+            F_1 <- ((RSS_env-R1_full)/1)/(R1_full/(pa-3))
+            pval_Y1 <- pf(F_1,1,(pa-2),lower.tail=FALSE)
+            
+            snp <- colnames(X_ok)
+            out_models <- data.frame(SNP=snp,Pval=pval_Y1,variance_explained=1-R1_full/RSS_env)
+        }
+        
+        output <- merge(MAF_ok,out_models,by='SNP')
+        
+        if(include.lm == TRUE) {
+            RSS_env_ <- rep(sum(lsfit(rep(1,length(Y)),Y,intercept = FALSE)$residuals^2),ncol(X_ok))
+            R1_full_ <- apply(X_ok,2,function(x){sum(lsfit(x,Y,intercept = T)$residuals^2)})
+            pa<-nrow(Y1)
+            
+            F_1_ <- ((RSS_env_-R1_full_)/1)/(R1_full_/(pa-3))
+            pval_Y1_lm <- pf(F_1_,1,(pa-3),lower.tail=FALSE)
+            
+            snp <- colnames(X_ok)
+            out_models_lm <- data.frame(SNP=snp,Pval_lm=pval_Y1_lm)
+            output <- merge(output,out_models_lm,by='SNP')
+        }
 
-snp<-colnames(X_ok)
-out_models_lm<-data.frame(SNP=snp,Pval_lm=pval_Y1_lm)
-output<-merge(output,out_models_lm,by='SNP')
-
+        if(include.kw == TRUE) {
+            kw <- apply(X_ok,2,function(x){kruskal.test(Y,x)$p.value})
+            KW <- cbind(names(kw),kw)
+            colnames(KW) <- c('SNP','Pval_kw')
+            output <- merge(output,KW,by='SNP')
+            
+        }
+        
+        ## update tp SNPs with correct model 
+        if(update.top_snps == FALSE) {
+            return(output)
+        } else {
+            oi <- output[order(output[,8]),][1:update.top_snps,1]
+            xs <- t(X_ok[,colnames(X_ok)%in%oi])
+            auto <- emma.ML.LRT (Y, xs, K_ok)
+            up <- data.frame(SNP=rownames(xs),update=auto$ps[,1])
+            
+            for(i in 1:update.top_snps) {
+                output[which(output[,1]==up[i,1]),]$Pval <- up[i,2]
+            }
+            return(output)
+        }
+    }
 }
-
-if (include.kw==TRUE) {
-  kw<-apply(X_ok,2,function(x){kruskal.test(Y,x)$p.value})
-  
-  KW<-cbind(names(kw),kw)
-  colnames(KW)<-c('SNP','Pval_kw')
-  output<-merge(output,KW,by='SNP')
-  
-}
-
-## update tp SNPs with correct model 
-if (update.top_snps==FALSE) {
-
-
-return(output) } else {
-
-oi<-output[order(output[,8]),][1:update.top_snps,1]
-
-
-xs=t(X_ok[,colnames(X_ok)%in%oi])
-
-
-auto<-emma.ML.LRT (Y, xs, K_ok)
-
-up<-data.frame(SNP=rownames(xs),update=auto$ps[,1])
-
- for ( i in 1: update.top_snps) {output[which(output[,1]==up[i,1]),]$Pval<-up[i,2]
-}
-
-return(output)
-}}}
 
 
 
